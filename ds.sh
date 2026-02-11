1ce2682d13afe4f40e4f8595b1b14d9815c53067
Rodion Efremov <coderodd3@yandex.com>
2026-02-11 09:14:41 +0200

    Refactor DSWITCH_SHELL assignment in ds.sh
    Removed export statement for DSWITCH_SHELL and adjusted its assignment.

--- ds.sh
+++ ds.sh
@@ -1,7 +1,6 @@
 rm -f ~/.dswitcher/ds_command.sh
-export DSWITCH_SHELL=bash
+DSWITCH_SHELL=bash
 ~/.dswitcher/dswitch $@
-unset DSWITCH_SHELL
 
 if [ -f ~/.dswitcher/ds_command.sh ]; then
     . ~/.dswitcher/ds_command.sh
